1411a7880128a5e192374846b37777e2418a9d49
nahayatim <mahtabnahayati@gmail.com>
2019-02-26 15:51:23 +0100

    the cursed coin

---
+++ springExercises/ex3-thecursedcoin/src/main/java/at/nacs/ex3thecursedcoin/CursedCoin.java
@@ -0,0 +1,13 @@
+package at.nacs.ex3thecursedcoin;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CursedCoin {
+
+    public String findCursedCoin(){
+
+        return "I found the CursedCoin!";
+    }
+
+}
---
+++ springExercises/ex3-thecursedcoin/src/main/java/at/nacs/ex3thecursedcoin/JackSparrow.java
@@ -0,0 +1,17 @@
+package at.nacs.ex3thecursedcoin;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class JackSparrow {
+
+    private final LegendaryCompass legendaryCompass;
+
+    public String findCursedCoin() {
+
+        return legendaryCompass.toFind();
+
+    }
+}

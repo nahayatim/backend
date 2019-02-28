1b0f596b8e9566ed8d3730127514debc6c5a8d4d
nahayatim <mahtabnahayati@gmail.com>
2019-02-28 15:02:23 +0100

    primeAdder test

--- reflection/week02/prime-number-adder/src/test/java/at/nacs/primenumberadder/PrimeAdderTest.java
+++ reflection/week02/prime-number-adder/src/test/java/at/nacs/primenumberadder/PrimeAdderTest.java
@@ -6,34 +6,44 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest(classes =  PrimeAdder.class)
-
+@SpringBootTest
 class PrimeAdderTest {
 
     @Autowired
-    private PrimeAdder primeAdder;
+    PrimeAdder primeAdder;
+
+    @Autowired
+    PrimeCollector primeCollector;
+
+    @Autowired
+    Primes primes;
 
     @ParameterizedTest
     @CsvSource({
-            " 10,new Arrays.asList(2,3,5)",
+            "5, 2, 3"
 //            "378282346310005, false",
 //            "371847579533915, true",
 //            "5114266492307268, true",
 //            "4417123456789113, true",
 //            "2437123456789113, false" //this is not a credit card number, but current implementation treats it like that
     })
-
-    @Test
-    void calculateSum(long expected, List<Integer>primeNumbers) {
+    void calculateSum(long expected, int number1, int number2) {
+        List<Integer> primeNumbers = List.of(number1, number2);
         long actual = primeAdder.calculateSum(primeNumbers);
+        System.out.println(primeNumbers);
 
         assertEquals(expected, actual);
+    }
 
+    @Test
+    void calculateSumOfHundredPrimes() {
+        List<Integer> primeNumbers = primeCollector.collectPrimeNumbers(100);
+        long actual = primeAdder.calculateSum(primeNumbers);
 
+        assertEquals(24133, actual);
     }
 }

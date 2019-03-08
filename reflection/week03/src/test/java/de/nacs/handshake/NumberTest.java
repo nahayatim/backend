64a62bc7a7a551179de2038d3ce0babae5dc765f
nahayatim <mahtabnahayati@gmail.com>
2019-03-08 14:01:12 +0100

    test splitter

--- reflection/week03/src/test/java/de/nacs/handshake/NumberTest.java
+++ reflection/week03/src/test/java/de/nacs/handshake/NumberTest.java
@@ -1,5 +1,39 @@
+package de.nacs.handshake;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class NumberTest {
+
+    @Autowired
+    Number number;
+
+    @ParameterizedTest
+    @CsvSource({
+            "1 - 2 - 4,124",
+            "4 - 5 - 6 - 7,4567",
+            "3 - 4,34"
+    })
+    void checkSplitter(String numbers, Integer digits) {
+        List<String> split = List.of(numbers.split(" - "));
+        List<Integer> expected = split.stream()
+                .map(e -> Integer.valueOf(e))
+                .collect(Collectors.toList());
+
+        List<Integer> actual = number.split(digits);
+        Assertions.assertEquals(expected, actual);
+
+    }
 
 }

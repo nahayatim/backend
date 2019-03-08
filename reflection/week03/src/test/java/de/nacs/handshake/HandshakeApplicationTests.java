9bc48fa4b03527c0fff7c9938225baae22a79e8f
nahayatim <mahtabnahayati@gmail.com>
2019-03-08 14:00:01 +0100

    application test

---
+++ reflection/week03/src/test/java/de/nacs/handshake/HandshakeApplicationTests.java
@@ -0,0 +1,16 @@
+package de.nacs.handshake;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HandshakeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

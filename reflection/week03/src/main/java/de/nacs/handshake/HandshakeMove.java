3386cd28bfa355e51e704c9c8016c6840bd05350
nahayatim <mahtabnahayati@gmail.com>
2019-03-08 13:49:59 +0100

    map of handshakemove

--- reflection/week03/src/main/java/de/nacs/handshake/HandshakeMove.java
+++ reflection/week03/src/main/java/de/nacs/handshake/HandshakeMove.java
@@ -1,4 +1,17 @@
 package de.nacs.handshake;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+@ConfigurationProperties("handshake")
 public class HandshakeMove {
+
+    @Setter
+    @Getter
+    private Map<Integer, String> handshakes;
 }

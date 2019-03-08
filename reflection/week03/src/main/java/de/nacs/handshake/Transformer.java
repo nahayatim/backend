b6329eb299870b3467a29ac695975ac7b935e94e
nahayatim <mahtabnahayati@gmail.com>
2019-03-08 13:52:25 +0100

    transform listOfNumber to listOfString

--- reflection/week03/src/main/java/de/nacs/handshake/Transformer.java
+++ reflection/week03/src/main/java/de/nacs/handshake/Transformer.java
@@ -5,21 +5,23 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @RequiredArgsConstructor
 @Component
-public class transformer {
+public class Transformer {
 
     private final Number number;
     private final HandshakeMove handshakeMove;
 
     public List<String> transform(Integer price) {
-        return Stream.of(number.split(price))
+        return number.split(price).stream()
                 .filter(handshakeMove.getHandshakes()::containsKey)
                 .map(handshakeMove.getHandshakes()::get)
+                .filter(Objects::nonNull)
                 .collect(Collectors.toList());
-
     }
 }

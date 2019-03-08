f7908af6e75f950a2147807d67fe7b40faa3ff5c
nahayatim <mahtabnahayati@gmail.com>
2019-03-08 13:51:07 +0100

    split price to list of number

--- reflection/week03/src/main/java/de/nacs/handshake/Number.java
+++ reflection/week03/src/main/java/de/nacs/handshake/Number.java
@@ -1,4 +1,22 @@
 package de.nacs.handshake;
 
+
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Component
 public class Number {
+
+    public List<Integer> split(Integer number) {
+        return Stream.of(number)
+                .map(n -> n.toString())
+                .map(n -> n.split(""))
+                .flatMap(Stream::of)
+                .map(n -> Integer.parseInt(n))
+                .collect(Collectors.toList());
+
+    }
 }

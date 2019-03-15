e07bb4c217132fa23dcd5c80b672a04379a8556e
nahayatim <mahtabnahayati@gmail.com>
2019-03-15 21:35:32 +0100

    morseEndpointTest

--- reflection/morse-encoder2/morse/src/test/java/at/nacs/morse/MorseEndpointTest.java
+++ reflection/morse-encoder2/morse/src/test/java/at/nacs/morse/MorseEndpointTest.java
@@ -1,5 +1,19 @@
-import static org.junit.jupiter.api.Assertions.*;
+package at.nacs.morse;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class MorseEndpointTest {
 
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Test
+    void post() {
+
+        Assertions.assertThat(restTemplate.postForObject("/morse","A",String.class));
+    }
 }

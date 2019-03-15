fb22432d3422806086fffcec0650d484a54e3ba9
nahayatim <mahtabnahayati@gmail.com>
2019-03-15 21:34:08 +0100

    encoderTest

--- reflection/morse-encoder2/morse/src/test/java/at/nacs/morse/EncoderTest.java
+++ reflection/morse-encoder2/morse/src/test/java/at/nacs/morse/EncoderTest.java
@@ -1,5 +1,22 @@
-import static org.junit.jupiter.api.Assertions.*;
+package at.nacs.morse;
 
+
+import at.nacs.morse.business.Encoder;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 class EncoderTest {
+
+    @Autowired
+    Encoder encoder;
+
+    @Test
+    void encode() {
+         Assertions.assertThat(encoder.encode("A").equals(".-"));
+
+    }
 
 }

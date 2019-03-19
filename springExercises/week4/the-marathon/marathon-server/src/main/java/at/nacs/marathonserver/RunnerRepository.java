d310e1423f58420fd06d5212cd8ad6885612dcd0
nahayatim <mahtabnahayati@gmail.com>
2019-03-19 12:41:43 +0100

    runnerRepository

--- springExercises/week4/the-marathon/marathon-server/src/main/java/at/nacs/marathonserver/RunnerRepository.java
+++ springExercises/week4/the-marathon/marathon-server/src/main/java/at/nacs/marathonserver/RunnerRepository.java
@@ -1,4 +1,25 @@
 package at.nacs.marathonserver;
 
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+
+@Component
 public class RunnerRepository {
+
+    private Set<Runner> runners = new HashSet<>();
+
+    public void add(Runner runner) {
+        runners.add(runner);
+    }
+
+    public List<Runner> findAll() {
+        return new ArrayList<>(runners);
+    }
+
+    public Optional<Runner> findWinner() {
+        return runners.stream()
+                .min(Comparator.comparing(runner -> runner.getTime()));
+    }
+
 }

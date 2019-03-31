df1512dcefacefb9677966fb4edfb44fb5f169cd
nahayatim <mahtabnahayati@gmail.com>
2019-03-31 23:05:49 +0200

    todoManager test

--- springExercises/week6/todos/src/test/java/at/nacs/todos/logic/ToDoManagerTest.java
+++ springExercises/week6/todos/src/test/java/at/nacs/todos/logic/ToDoManagerTest.java
@@ -1,9 +1,16 @@
 package at.nacs.todos.logic;
 
 import at.nacs.todos.persistence.ToDo;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
@@ -15,32 +22,58 @@ class ToDoManagerTest {
     ToDo toDo;
 
     @BeforeEach
-    void before(){
+    void before() {
         toDoManager.deleteAll();
         ToDo toDo = ToDo.builder().title("Test").build();
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void findAll() {
+        List<ToDo> todos = toDoManager.findAll();
+        assertThat(todos).isEmpty();
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void findOne() {
+        toDoManager.save(toDo);
+
+        Optional<ToDo> actual = toDoManager.findOne(toDo.getId());
+        actual.ifPresent(e -> assertThat(e).isEqualTo(toDo));
+
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void save() {
+        toDoManager.save(toDo);
+
+        Optional<ToDo> actual = toDoManager.findOne(toDo.getId());
+        actual.ifPresent(e -> assertThat(e).isEqualTo(toDo));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void markAsDone() {
+        toDoManager.save(toDo);
+        String id = toDo.getId();
+
+        toDoManager.markAsDone(id);
+
+        Optional<ToDo> actual = toDoManager.findOne(id);
+        actual.ifPresent(e -> assertThat(e.isDone()).isTrue());
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void delete() {
+        toDoManager.save(toDo);
+        String id = toDo.getId();
+        assertThat(toDo.getId()).isNotBlank();
+
+        toDoManager.delete(id);
+
+        Optional<ToDo> actual = toDoManager.findOne(id);
+        assertThat(actual.isPresent()).isFalse();
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void deleteAll() {
     }
 }

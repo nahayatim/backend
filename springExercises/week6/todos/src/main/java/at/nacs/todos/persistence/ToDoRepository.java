928d55b7f0dc3a482a52427d85272982dc348248
nahayatim <mahtabnahayati@gmail.com>
2019-03-31 23:06:54 +0200

    todos

--- springExercises/week6/todos/src/main/java/at/nacs/todos/persistence/ToDoRepository.java
+++ springExercises/week6/todos/src/main/java/at/nacs/todos/persistence/ToDoRepository.java
@@ -1,6 +1,5 @@
 package at.nacs.todos.persistence;
 
-import at.nacs.todos.persistence.ToDo;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ToDoRepository extends MongoRepository<ToDo, String> {

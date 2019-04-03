9119a65be7953a838ca12aebb5a889e362fb3dcd
nahayatim <mahtabnahayati@gmail.com>
2019-04-03 22:51:01 +0200

    domain

--- springExercises/week7/social-network/src/main/java/at/nacs/socialnetwork/persistence/Person.java
+++ springExercises/week7/social-network/src/main/java/at/nacs/socialnetwork/persistence/Person.java
@@ -1,4 +1,35 @@
-package at.nacs.socialnetwork;
+package at.nacs.socialnetwork.persistence;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Entity
+@Data
+@EqualsAndHashCode(exclude = "friends")
 public class Person {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    @ManyToMany(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+    private List<Person> friends=new ArrayList<>();
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", friends=" + friends.stream()
+                .map(Person::getName)
+                .collect(Collectors.toList())+
+                '}';
+    }
 }

019d1023e7e611dc35cfa7d0c15e997248c9161d
nahayatim <mahtabnahayati@gmail.com>
2019-04-03 22:50:26 +0200

    corsConfiguration

--- springExercises/week7/social-network/src/main/java/at/nacs/socialnetwork/configuration/CorsConfiguration.java
+++ springExercises/week7/social-network/src/main/java/at/nacs/socialnetwork/configuration/CorsConfiguration.java
@@ -1,4 +1,4 @@
-package at.nacs.phonebook.configuration;
+package at.nacs.socialnetwork.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;

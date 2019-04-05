cdeaee4a4d42d9684fdd64eec8a119b7d785a1d6
nahayatim <mahtabnahayati@gmail.com>
2019-04-05 18:42:08 +0200

    AccountancyClient

--- reflection/doctor-house-hospital/beds/src/main/java/at/nacs/beds/communication/AccountancyClient.java
+++ reflection/doctor-house-hospital/beds/src/main/java/at/nacs/beds/communication/AccountancyClient.java
@@ -1,4 +1,25 @@
 package at.nacs.beds.communication;
 
+import at.nacs.beds.persistence.domain.Patient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequiredArgsConstructor
 public class AccountancyClient {
+
+    private final RestTemplate restTemplate;
+
+    @Value("${patient.url}")
+    private String url;
+
+
+    public void forward(Patient patient) {
+
+        restTemplate.postForObject(url, patient, Patient.class);
+
+    }
+
 }

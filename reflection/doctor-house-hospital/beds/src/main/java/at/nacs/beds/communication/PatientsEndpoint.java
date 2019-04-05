e46ad5ba84e3bb739f97b17780286ddb9a74e296
nahayatim <mahtabnahayati@gmail.com>
2019-04-05 18:42:43 +0200

    PatientsEndpoint

--- reflection/doctor-house-hospital/beds/src/main/java/at/nacs/beds/communication/PatientsEndpoint.java
+++ reflection/doctor-house-hospital/beds/src/main/java/at/nacs/beds/communication/PatientsEndpoint.java
@@ -1,4 +1,23 @@
 package at.nacs.beds.communication;
 
+import at.nacs.beds.logic.PatientManager;
+import at.nacs.beds.persistence.domain.Patient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/patients")
+@RequiredArgsConstructor
 public class PatientsEndpoint {
+
+    private final PatientManager patientManager;
+
+    @PostMapping
+    Patient post(@RequestBody Patient patient) {
+        patientManager.register(patient);
+        return patient;
+    }
 }

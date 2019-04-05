5f04328a6d360bc8bfe3fb5158869afed05b97f1
nahayatim <mahtabnahayati@gmail.com>
2019-04-05 18:48:50 +0200

    logic

--- reflection/doctor-house-hospital/beds/src/main/java/at/nacs/beds/logic/Nurse.java
+++ reflection/doctor-house-hospital/beds/src/main/java/at/nacs/beds/logic/Nurse.java
@@ -1,4 +1,28 @@
 package at.nacs.beds.logic;
 
+import at.nacs.beds.persistence.domain.Patient;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@Service
+@ConfigurationProperties("patient")
 public class Nurse {
+
+    @Getter
+    @Setter
+    private Map<String, String> treatment;
+
+
+    public Patient provideTreatment(Patient patient) {
+        String diagnosis = patient.getDiagnosis();
+        patient.setTreatment(diagnosis);
+        return patient;
+
+    }
 }

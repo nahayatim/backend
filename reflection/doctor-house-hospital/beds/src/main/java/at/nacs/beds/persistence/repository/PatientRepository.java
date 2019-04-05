6ec220a0d10c53e6a3bb6242bdeb28c2b53a0c75
nahayatim <mahtabnahayati@gmail.com>
2019-04-05 18:52:43 +0200

    patientRepository

--- reflection/doctor-house-hospital/beds/src/main/java/at/nacs/beds/persistence/repository/PatientRepository.java
+++ reflection/doctor-house-hospital/beds/src/main/java/at/nacs/beds/persistence/repository/PatientRepository.java
@@ -1,4 +1,8 @@
 package at.nacs.beds.persistence.repository;
 
-public class PatientRepository {
+
+import at.nacs.beds.persistence.domain.Patient;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface PatientRepository extends MongoRepository<Patient, String> {
 }

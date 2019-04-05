92cc0c01b01ae2fa44247abc32e68a625e2c07ed
nahayatim <mahtabnahayati@gmail.com>
2019-04-05 18:52:03 +0200

    Patient

--- reflection/doctor-house-hospital/beds/src/main/java/at/nacs/beds/persistence/domain/Patient.java
+++ reflection/doctor-house-hospital/beds/src/main/java/at/nacs/beds/persistence/domain/Patient.java
@@ -1,4 +1,19 @@
 package at.nacs.beds.persistence.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Patient {
+
+    private String id;
+    private String name;
+    private String symptoms;
+    private String diagnosis;
+    private String treatment;
 }

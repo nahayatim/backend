ddbf5a52651482c4d837a015e3cb23f61cbb8099
nahayatim <mahtabnahayati@gmail.com>
2019-04-06 22:36:52 +0200

    ContactsConfiguration

--- springExercises/week7/phone-book/src/test/java/at/nacs/phonebook/dataset/ContactsConfiguration.java
+++ springExercises/week7/phone-book/src/test/java/at/nacs/phonebook/dataset/ContactsConfiguration.java
@@ -1,4 +1,24 @@
 package at.nacs.phonebook.dataset;
 
+import at.nacs.phonebook.persistence.domain.Contact;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+@ConfigurationProperties("dataset")
 public class ContactsConfiguration {
+
+    @Getter
+    @Setter
+    List<Contact> contacts;
+
+    @Bean
+    List<Contact> contacts() {
+        return contacts;
+    }
 }

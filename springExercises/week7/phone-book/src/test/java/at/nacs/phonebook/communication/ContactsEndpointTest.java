e9f0cbeb5ef57d14a445bfed15797f43220d8eb5
nahayatim <mahtabnahayati@gmail.com>
2019-04-06 22:39:43 +0200

    ContactsEndpointTest

--- springExercises/week7/phone-book/src/test/java/at/nacs/phonebook/communication/ContactsEndpointTest.java
+++ springExercises/week7/phone-book/src/test/java/at/nacs/phonebook/communication/ContactsEndpointTest.java
@@ -1,5 +1,65 @@
-import static org.junit.jupiter.api.Assertions.*;
+package at.nacs.phonebook.communication;
 
+import at.nacs.phonebook.logic.ContactManager;
+import at.nacs.phonebook.persistence.domain.Contact;
+import at.nacs.phonebook.persistence.repository.ContactRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.springframework.boot.test.context.SpringBootTest.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.*;
+
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class ContactsEndpointTest {
 
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
+    @Autowired
+    List<Contact>contacts;
+
+    @SpyBean
+    ContactManager contactManager;
+
+    @MockBean
+    ContactRepository contactRepository;
+
+
+    final String url="/contacts";
+
+
+    @Test
+    void get() {
+        testRestTemplate.getForObject(url,Contact[].class);
+
+        verify(contactManager).findAll();
+    }
+
+    @Test
+    void post() {
+        Contact contact = contacts.get(0);
+        testRestTemplate.postForObject(url,contact,Contact.class);
+
+        verify(contactManager).save(contact);
+    }
+
+    @Test
+    void getByAddress() {
+        String address="fake address";
+        String getByAddressURL= url+"/address"+address;
+
+        testRestTemplate.getForObject(getByAddressURL,Contact[].class);
+
+        verify(contactManager).findLike(address);
+    }
 }
--- springExercises/week7/phone-book/src/test/java/at/nacs/phonebook/configuration/ContactConfiguration.java
+++
@@ -1,4 +0,0 @@
-package at.nacs.phonebook.configuration;
-
-public class ContactConfiguration {
-}

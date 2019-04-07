b8ea167d6ad96db2a19489475ba75c607d66bcac
nahayatim <mahtabnahayati@gmail.com>
2019-04-07 19:14:42 +0200

    PatientEndpoint test

---
+++ reflection/doctor-house-hospital/beds/src/test/java/at/nacs/beds/TreatmentConfiguration.java
@@ -0,0 +1,4 @@
+package at.nacs.beds;
+
+public class TreatmentConfiguration {
+}
---
+++ reflection/doctor-house-hospital/beds/src/test/java/at/nacs/beds/communication/PatientsEndpointTest.java
@@ -0,0 +1,41 @@
+package at.nacs.beds.communication;
+
+import at.nacs.beds.logic.Nurse;
+import at.nacs.beds.persistence.domain.Patient;
+import at.nacs.beds.persistence.repository.PatientRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.*;
+
+@SpringBootTest(webEnvironment= RANDOM_PORT)
+class PatientsEndpointTest {
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @MockBean
+    AccountancyClient accountancyClient;
+
+    @MockBean
+    PatientRepository patientRepository;
+
+    @SpyBean
+    Nurse nurse;
+
+    String url= "/patients";
+
+
+    @Test
+    void post() {
+        Patient patient = Patient.builder().name("ani").symptoms("cough").diagnosis("cold").build();
+        restTemplate.postForObject(url,patient,Patient.class);
+        Mockito.verify(nurse).provideTreatment(patient);
+
+    }
+}

8acaed692627dfe8ab2722c9a13234383eb5246a
nahayatim <mahtabnahayati@gmail.com>
2019-04-07 20:55:45 +0200

    configuration

--- reflection/doctor-house-hospital/pharmacy/src/main/java/at/nacs/drhousepharmacy/configuration/CorsConfiguration.java
+++ reflection/doctor-house-hospital/pharmacy/src/main/java/at/nacs/drhousepharmacy/configuration/CorsConfiguration.java
@@ -1,4 +1,4 @@
-package at.nacs.trickster.configuration;
+package at.nacs.drhousepharmacy.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,8 +7,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
 
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
     }
 }
--- reflection/doctor-house-hospital/pharmacy/src/main/java/at/nacs/drhousepharmacy/configuration/RestTemplateConfiguration.java
+++ reflection/doctor-house-hospital/pharmacy/src/main/java/at/nacs/drhousepharmacy/configuration/RestTemplateConfiguration.java
@@ -1,4 +1,4 @@
-package at.nacs.marathonclient.configuration;
+package at.nacs.drhousepharmacy.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;

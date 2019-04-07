bcf6846c37bf738bbb51103bb290429735643cd8
nahayatim <mahtabnahayati@gmail.com>
2019-04-07 20:56:24 +0200

    Apothecary

--- reflection/doctor-house-hospital/pharmacy/src/main/java/at/nacs/drhousepharmacy/logic/Apothecary.java
+++ reflection/doctor-house-hospital/pharmacy/src/main/java/at/nacs/drhousepharmacy/logic/Apothecary.java
@@ -1,4 +1,24 @@
 package at.nacs.drhousepharmacy.logic;
 
+import at.nacs.drhousepharmacy.communication.AccountancyClient;
+import at.nacs.drhousepharmacy.persistence.domain.Patient;
+import at.nacs.drhousepharmacy.persistence.repository.PatientRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class Apothecary {
+
+    private final PatientRepository patientRepository;
+    private final AccountancyClient accountancyClient;
+
+
+    public Patient provideMedicine(Patient patient) {
+        patient.setMedicine("metformin");
+        accountancyClient.forward(patient);
+        patientRepository.save(patient);
+        return patient;
+
+    }
 }

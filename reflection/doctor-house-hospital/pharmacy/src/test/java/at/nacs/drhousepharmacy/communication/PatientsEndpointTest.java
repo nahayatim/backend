46ca665b0b0a115711dfd88893947d5aa5988d61
nahayatim <mahtabnahayati@gmail.com>
2019-04-07 20:57:55 +0200

    PatientEndpoint test

--- reflection/doctor-house-hospital/pharmacy/src/test/java/at/nacs/drhousepharmacy/communication/PatientsEndpointTest.java
+++ reflection/doctor-house-hospital/pharmacy/src/test/java/at/nacs/drhousepharmacy/communication/PatientsEndpointTest.java
@@ -1,5 +1,44 @@
-import static org.junit.jupiter.api.Assertions.*;
+package at.nacs.drhousepharmacy.communication;
 
+
+import at.nacs.drhousepharmacy.logic.Apothecary;
+import at.nacs.drhousepharmacy.persistence.domain.Patient;
+import at.nacs.drhousepharmacy.persistence.repository.PatientRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class PatientsEndpointTest {
 
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Autowired
+    PatientsEndpoint patientsEndpoint;
+
+    @MockBean
+    PatientRepository patientRepository;
+
+    @MockBean
+    AccountancyClient accountancyClient;
+
+    @SpyBean
+    Apothecary apothecary;
+
+    String url="/patients";
+
+    @Test
+    void post() {
+        Patient patient = Patient.builder().diagnosis("Cough").build();
+        restTemplate.postForObject(url,patient,Patient.class);
+        Mockito.verify(apothecary).provideMedicine(patient);
+
+    }
 }

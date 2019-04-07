4b4f8db5cccdcdba3d716a29783a3216d138551c
nahayatim <mahtabnahayati@gmail.com>
2019-04-07 19:15:19 +0200

    Nurse test

--- reflection/doctor-house-hospital/beds/src/test/java/at/nacs/beds/logic/NurseTest.java
+++ reflection/doctor-house-hospital/beds/src/test/java/at/nacs/beds/logic/NurseTest.java
@@ -1,5 +1,35 @@
-import static org.junit.jupiter.api.Assertions.*;
+package at.nacs.beds.logic;
 
+import at.nacs.beds.communication.AccountancyClient;
+import at.nacs.beds.persistence.domain.Patient;
+import at.nacs.beds.persistence.repository.PatientRepository;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.*;
+
+@SpringBootTest(webEnvironment = WebEnvironment.NONE)
 class NurseTest {
+
+    @MockBean
+    AccountancyClient accountancyClient;
+
+    @Autowired
+    Nurse nurse;
+
+    @Autowired
+    PatientRepository repository;
+
+    @Test
+    void register() {
+        Patient patient = Patient.builder().diagnosis("cold").build();
+        nurse.provideTreatment(patient);
+        assertThat(patient.getTreatment()).isEqualTo("mexalen");
+    }
 
 }

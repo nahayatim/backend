c5e9965a85e847390bb7402a5233af276d77d25a
nahayatim <mahtabnahayati@gmail.com>
2019-04-11 21:26:49 +0200

    add patientDTO

--- reflection/doctor-house-hospital/accountancy/src/main/java/at/nacs/drhouseaccountancy/dto/PatientDTO.java
+++ reflection/doctor-house-hospital/accountancy/src/main/java/at/nacs/drhouseaccountancy/dto/PatientDTO.java
@@ -1,4 +1,15 @@
 package at.nacs.drhouseaccountancy.dto;
 
+import lombok.Data;
+
+
+@Data
 public class PatientDTO {
+
+  private String id;
+  private String name;
+  private String symptoms;
+  private String diagnosis;
+  private String treatment;
+  private String medicine;
 }

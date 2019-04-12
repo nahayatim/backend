347caea27cb665a4880fa0c3e6fe4d97cb15dd8b
nahayatim <mahtabnahayati@gmail.com>
2019-04-12 22:47:07 +0200

    accountant added

--- reflection/doctor-house-hospital/accountancy/src/main/java/at/nacs/drhouseaccountancy/logic/Accountant.java
+++ reflection/doctor-house-hospital/accountancy/src/main/java/at/nacs/drhouseaccountancy/logic/Accountant.java
@@ -1,4 +1,37 @@
 package at.nacs.drhouseaccountancy.logic;
 
+import at.nacs.drhouseaccountancy.dto.PatientDTO;
+import at.nacs.drhouseaccountancy.persistence.domain.Invoice;
+import at.nacs.drhouseaccountancy.persistence.domain.Patient;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@ConfigurationProperties("dataset")
+@Service
+@RequiredArgsConstructor
 public class Accountant {
+
+  private final PatientManager patientManager;
+  private final InvoiceCreator invoiceCreator;
+  private final InvoiceManager invoiceManager;
+
+  @Setter
+  private Map<String, Double> costs;
+
+  public void post(PatientDTO patientDTO) {
+    Patient patient = patientManager.create(patientDTO);
+    Invoice invoice = invoiceCreator.createInvoice(patientDTO, patient);
+    Double costs = calculateCosts(invoice);
+    invoice.setCost(costs);
+    patientManager.save(patient);
+    invoiceManager.storeInvoice(invoice);
+  }
+
+  private Double calculateCosts(Invoice invoice) {
+    return costs.getOrDefault(invoice.getProvided(), null);
+  }
 }

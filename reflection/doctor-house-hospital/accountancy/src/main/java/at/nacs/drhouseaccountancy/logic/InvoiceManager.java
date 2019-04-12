751a2d95e293380bd2934d4e181a72e7c1108c47
nahayatim <mahtabnahayati@gmail.com>
2019-04-12 22:23:10 +0200

    InvoiceManager added

--- reflection/doctor-house-hospital/accountancy/src/main/java/at/nacs/drhouseaccountancy/logic/InvoiceManager.java
+++ reflection/doctor-house-hospital/accountancy/src/main/java/at/nacs/drhouseaccountancy/logic/InvoiceManager.java
@@ -1,4 +1,31 @@
 package at.nacs.drhouseaccountancy.logic;
 
+import at.nacs.drhouseaccountancy.persistence.domain.Invoice;
+import at.nacs.drhouseaccountancy.persistence.repository.InvoiceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class InvoiceManager {
+
+  private final InvoiceRepository invoiceRepository;
+
+
+  public void storeInvoice(Invoice invoice) {
+    invoiceRepository.save(invoice);
+  }
+
+  public List<Invoice> findAllAvailableInvoice() {
+    return invoiceRepository.findAll();
+  }
+
+  public void setPaidInvoice(Long id) {
+    Invoice invoice = invoiceRepository.findById(id).orElse(null);
+    invoice.setPaid(true);
+    invoiceRepository.save(invoice);
+  }
+
 }

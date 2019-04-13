ce3450067764b7b154c0ffc3ac933fe7f8ee327a
nahayatim <mahtabnahayati@gmail.com>
2019-04-13 09:52:42 +0200

    patientEndpoint added

--- reflection/doctor-house-hospital/accountancy/src/main/java/at/nacs/drhouseaccountancy/communication/PatientEndpoint.java
+++ reflection/doctor-house-hospital/accountancy/src/main/java/at/nacs/drhouseaccountancy/communication/PatientEndpoint.java
@@ -1,4 +1,23 @@
 package at.nacs.drhouseaccountancy.communication;
 
+import at.nacs.drhouseaccountancy.dto.PatientDTO;
+import at.nacs.drhouseaccountancy.logic.Accountant;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/patients")
+@RequiredArgsConstructor
+@RestController
 public class PatientEndpoint {
+
+  private final Accountant accountant;
+
+  @PostMapping
+  public PatientDTO post(@RequestBody PatientDTO patientDTO) {
+    accountant.post(patientDTO);
+    return patientDTO;
+  }
 }

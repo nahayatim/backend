55a61b50bd2ddea8f1a56e8bdaceb59e81d82361
nahayatim <mahtabnahayati@gmail.com>
2019-04-13 09:41:23 +0200

    invoiceEndpoint added

--- reflection/doctor-house-hospital/accountancy/src/main/java/at/nacs/drhouseaccountancy/communication/InvoiceEndpoint.java
+++ reflection/doctor-house-hospital/accountancy/src/main/java/at/nacs/drhouseaccountancy/communication/InvoiceEndpoint.java
@@ -1,4 +1,28 @@
 package at.nacs.drhouseaccountancy.communication;
 
+import at.nacs.drhouseaccountancy.logic.InvoiceManager;
+import at.nacs.drhouseaccountancy.persistence.domain.Invoice;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/invoices")
+@RestController
+@RequiredArgsConstructor
 public class InvoiceEndpoint {
+
+  private final InvoiceManager invoiceManager;
+
+  @GetMapping
+  public List<Invoice> findAll() {
+    return invoiceManager.findAllAvailableInvoice();
+  }
+
+  @PutMapping("/{id}/paid")
+  public void setPaidInvoice(@PathVariable Long id) {
+    invoiceManager.setPaidInvoice(id);
+  }
+
+
 }

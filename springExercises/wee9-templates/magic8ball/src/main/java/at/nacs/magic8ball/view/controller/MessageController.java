f9d912ed42415f32f19da25906873bb2d83c8ee4
nahayatim <mahtabnahayati@gmail.com>
2019-04-16 14:41:42 +0200

    messageController added

--- springExercises/wee9-templates/magic8ball/src/main/java/at/nacs/magic8ball/view/controller/MessageController.java
+++ springExercises/wee9-templates/magic8ball/src/main/java/at/nacs/magic8ball/view/controller/MessageController.java
@@ -1,4 +1,31 @@
-package at.nacs.magic8ball;
+package at.nacs.magic8ball.view.controller;
 
+import at.nacs.magic8ball.view.model.Message;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.*;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/")
 public class MessageController {
+
+  private final List<String> messages;
+
+  @ModelAttribute("messages")
+  Message getRandomMessage() {
+    Collections.shuffle(messages);
+    return Message.builder()
+                  .message(messages.get(0))
+                  .build();
+  }
+
+  @GetMapping
+  String page() {
+    return "message";
+  }
 }

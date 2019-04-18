66cc430635443f5df1fec98886954672dac556cd
nahayatim <mahtabnahayati@gmail.com>
2019-04-18 22:02:45 +0200

    Patient added

--- reflection/doctor-house-hospital/administration/src/main/java/at/nacs/drhouseadministration/view/model/Patient.java
+++ reflection/doctor-house-hospital/administration/src/main/java/at/nacs/drhouseadministration/view/model/Patient.java
@@ -1,4 +1,19 @@
 package at.nacs.drhouseadministration.view.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Patient {
+
+  @NotEmpty
+  private String name;
+
+  @NotEmpty
+  private String symptoms;
 }

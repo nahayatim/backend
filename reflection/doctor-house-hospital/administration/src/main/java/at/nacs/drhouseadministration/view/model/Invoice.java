897a94e8f4a87e88d9f950c5606934417388796e
nahayatim <mahtabnahayati@gmail.com>
2019-04-18 22:02:07 +0200

    Invoice added

--- reflection/doctor-house-hospital/administration/src/main/java/at/nacs/drhouseadministration/view/model/Invoice.java
+++ reflection/doctor-house-hospital/administration/src/main/java/at/nacs/drhouseadministration/view/model/Invoice.java
@@ -1,4 +1,22 @@
 package at.nacs.drhouseadministration.view.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Invoice {
+
+  private Long id;
+  private Patient patient;
+  private String provided;
+  private double cost;
+  private boolean paid;
+  private LocalDateTime timestamp;
+  private String symptoms;
+  private String diagnose;
 }

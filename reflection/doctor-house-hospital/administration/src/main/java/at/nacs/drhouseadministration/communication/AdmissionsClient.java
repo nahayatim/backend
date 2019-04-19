25f179b9986762f448e9699d4ba7cb45c2f18b76
nahayatim <mahtabnahayati@gmail.com>
2019-04-19 11:34:55 +0200

    AdmissionsClient added

--- reflection/doctor-house-hospital/administration/src/main/java/at/nacs/drhouseadministration/communication/AdmissionsClient.java
+++ reflection/doctor-house-hospital/administration/src/main/java/at/nacs/drhouseadministration/communication/AdmissionsClient.java
@@ -1,4 +1,21 @@
 package at.nacs.drhouseadministration.communication;
 
+import at.nacs.drhouseadministration.view.model.Patient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@RequiredArgsConstructor
 public class AdmissionsClient {
+
+  private final RestTemplate restTemplate;
+
+  @Value("${admissions.url}")
+  private String url;
+
+  public void admit(Patient patient) {
+    restTemplate.postForObject(url, patient, Patient.class);
+  }
 }

9a845d6c930f292cb177798d792629207eabd403
nahayatim <mahtabnahayati@gmail.com>
2019-04-19 11:34:24 +0200

    AccountancyClient added

--- reflection/doctor-house-hospital/administration/src/main/java/at/nacs/drhouseadministration/communication/AccountancyClient.java
+++ reflection/doctor-house-hospital/administration/src/main/java/at/nacs/drhouseadministration/communication/AccountancyClient.java
@@ -1,4 +1,27 @@
 package at.nacs.drhouseadministration.communication;
 
+import at.nacs.drhouseadministration.view.model.Invoice;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@RequiredArgsConstructor
 public class AccountancyClient {
+
+  private final RestTemplate restTemplate;
+
+
+  @Value("${accountancy.url}")
+  private String url;
+
+  public Invoice[] getAllInvoices() {
+    return restTemplate.getForObject(url, Invoice[].class);
+  }
+
+  public void PaidBy(String id) {
+    String urlPaid = url + "/" + id + "/paid";
+    restTemplate.put(urlPaid, void.class);
+  }
 }

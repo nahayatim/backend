9866fdf72b7674d73afd303e9e9bd198497b0d3c
nahayatim <mahtabnahayati@gmail.com>
2019-04-19 10:37:04 +0200

    Invoice updated

--- reflection/doctor-house-hospital/administration/src/main/java/at/nacs/drhouseadministration/view/model/Invoice.java
+++ reflection/doctor-house-hospital/administration/src/main/java/at/nacs/drhouseadministration/view/model/Invoice.java
@@ -13,10 +13,11 @@ public class Invoice {
 
   private Long id;
   private Patient patient;
+  private Kind kind;
+  private String symptoms;
+  private String diagnose;
   private String provided;
   private double cost;
   private boolean paid;
   private LocalDateTime timestamp;
-  private String symptoms;
-  private String diagnose;
 }

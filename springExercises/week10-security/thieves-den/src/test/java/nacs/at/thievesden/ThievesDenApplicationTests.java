0a1d9b5d6f41b15a57044077162160285990d022
nahayatim <mahtabnahayati@gmail.com>
2019-04-23 22:23:45 +0200

    thieves-den application added

---
+++ springExercises/week10-security/thieves-den/src/test/java/nacs/at/thievesden/ThievesDenApplicationTests.java
@@ -0,0 +1,16 @@
+package nacs.at.thievesden;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThievesDenApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}

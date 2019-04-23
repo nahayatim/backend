9ad63fe65acd7b88c64fa2c711f42d089960e039
nahayatim <mahtabnahayati@gmail.com>
2019-04-23 23:05:09 +0200

    error page added

---
+++ springExercises/week10-security/hotel-room-safe/src/main/java/nacs/at/hotelroomsafe/configuration/WebMvcConfiguration.java
@@ -0,0 +1,18 @@
+package nacs.at.hotelroomsafe.configuration;
+
+import org.springframework.context.annotation.Configuration;
+    import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+    import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+  @Override
+  public void addViewControllers(ViewControllerRegistry registry) {
+    registry.addViewController("/login").setViewName("login");
+    registry.addViewController("/").setViewName("home");
+    registry.addViewController("/user").setViewName("guest");
+  }
+}
+
+

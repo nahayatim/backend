9e34e3f33ca43ba96b9ae214eff95f5404ebf924
nahayatim <mahtabnahayati@gmail.com>
2019-06-11 11:44:57 +0200

    calculator added

--- code-review/calculator/src/Calculator.java
+++ code-review/calculator/src/Calculator.java
@@ -1,19 +1,38 @@
 public class Calculator {
 
+
   public double calculate(double num1, double num2, String operator) {
     if (operator.equals("+")) {
-      return (num1 + num2);
+     return Add(num1, num2);
     }
     if (operator.equals("-")) {
-      return num1 - num2;
+      return subtract(num1, num2);
     }
     if (operator.equals("*")) {
-      return num1 * num2;
+     return multiply(num1, num2);
     }
     if (operator.equals("/")) {
-      return num1 / num2;
+      return divide(num1, num2);
     }
-    throw new IllegalArgumentException("The operator is not supported:" + operator);
+
+    else
+      throw new IllegalArgumentException("The operator is not supported."+operator);
+  }
+
+
+  private double divide(double num1, double num2) {
+    return (num1 / num2);
+  }
+
+  private double multiply(double num1, double num2) {
+    return (num1 * num2);
+  }
+
+  private double subtract(double num1, double num2) {
+    return (num1 - num2);
+  }
+
+  private double Add(double num1, double num2) {
+    return (num1 + num2);
   }
 }
-

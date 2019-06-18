b62743e67fc5ca6f69e59c4d7614906f589a9d90
nahayatim <mahtabnahayati@gmail.com>
2019-06-18 11:17:32 +0200

    update ex2

--- springExercises/week2/ex2-amazinghelloworld/src/main/java/at/nacs/ex2amazinghelloworld/AmazingHelloWorld.java
+++ springExercises/week2/ex2-amazinghelloworld/src/main/java/at/nacs/ex2amazinghelloworld/AmazingHelloWorld.java
@@ -1,12 +1,17 @@
 package at.nacs.ex2amazinghelloworld;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class AmazingHelloWorld {
 
+  private final Hello hello;
+  private final World world;
 
-    public String say(){
-        return "Hello "+"World";
-    }
+
+  public String say() {
+    return hello.say() + " " + world.say();
+  }
 }

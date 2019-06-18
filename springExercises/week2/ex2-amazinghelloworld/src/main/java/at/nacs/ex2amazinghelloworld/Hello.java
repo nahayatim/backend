d4861fb9406276b62ced961feb557c6093fbfafd
nahayatim <mahtabnahayati@gmail.com>
2019-06-18 11:14:48 +0200

    update Hello class

---
+++ springExercises/week2/ex2-amazinghelloworld/src/main/java/at/nacs/ex2amazinghelloworld/Hello.java
@@ -0,0 +1,11 @@
+package at.nacs.ex2amazinghelloworld;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Hello {
+
+  public String say() {
+    return "Hello";
+  }
+}

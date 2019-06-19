b2471a3b03c04de9f2e913ebfa3f81af3507347d
nahayatim <mahtabnahayati@gmail.com>
2019-06-19 11:58:08 +0200

    cigar test added

--- springExercises/week2/ex4-theenglishgentleman/src/test/java/at/nacs/ex4theenglishgentleman/CigarTest.java
+++ springExercises/week2/ex4-theenglishgentleman/src/test/java/at/nacs/ex4theenglishgentleman/CigarTest.java
@@ -4,25 +4,27 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class CigarTest {
 
-    @Autowired
-    Cigar cigar;
+  @Autowired
+  Cigar cigar;
 
-    @Test
-    void smokeSucceeds() {
-        assertFalse(cigar.isLit());
+  @Test
+  void smokeSucceeds() throws CigarWasNotLitException {
+    assertFalse(cigar.isLit());
+    cigar.setLit(true);
+    cigar.smoke();
+  }
 
-        cigar.setLit(true);
-        assertDoesNotThrow(() -> cigar.smoke());
-    }
+  @Test
+  void smokeFails() {
+    assertFalse(cigar.isLit());
+    assertThrows(CigarWasNotLitException.class, () -> cigar.smoke());
+  }
 
-    @Test
-    void smokeFails() {
-        assertFalse(cigar.isLit());
-        assertThrows(CigarWasNotLitException.class, () -> cigar.smoke());
-    }
+
 }

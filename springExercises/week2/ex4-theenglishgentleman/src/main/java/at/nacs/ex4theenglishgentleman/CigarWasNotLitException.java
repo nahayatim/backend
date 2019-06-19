5af3a26bc25839d36aac733a9cfbd753e29b6811
nahayatim <mahtabnahayati@gmail.com>
2019-06-19 11:54:45 +0200

    Exception class added

--- springExercises/week2/ex4-theenglishgentleman/src/main/java/at/nacs/ex4theenglishgentleman/CigarWasNotLitException.java
+++ springExercises/week2/ex4-theenglishgentleman/src/main/java/at/nacs/ex4theenglishgentleman/CigarWasNotLitException.java
@@ -1,4 +1,10 @@
 package at.nacs.ex4theenglishgentleman;
 
 public class CigarWasNotLitException extends Exception {
+
+  public CigarWasNotLitException(String message) {
+
+    super(message);
+  }
+
 }

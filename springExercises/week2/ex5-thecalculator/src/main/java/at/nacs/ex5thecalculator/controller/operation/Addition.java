ec2d612395b4fc9eb669e4b24216c3dcc9e607e5
nahayatim <mahtabnahayati@gmail.com>
2019-06-21 15:38:02 +0200

    Addition operation added

---
+++ springExercises/week2/ex5-thecalculator/src/main/java/at/nacs/ex5thecalculator/controller/operation/Addition.java
@@ -0,0 +1,18 @@
+package at.nacs.ex5thecalculator.controller.operation;
+
+import at.nacs.ex5thecalculator.model.Expression;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Addition implements Operation {
+
+  @Override
+  public boolean matches(Expression expression) {
+    return expression.getSymbol().equals("+");
+  }
+
+  @Override
+  public double apply(Expression expression) {
+    return expression.getNumber1() + expression.getNumber2();
+  }
+}
